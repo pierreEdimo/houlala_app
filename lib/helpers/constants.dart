5fa7958a3f31d13d7feb47dceb602cb13735e552
pnk <pierre.nkoe@vema-eg.de>
2024-10-14 23:43:15 +0200

    Implementation des constants

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const verticalPadding = 25.0;
+const horizontalPadding = 11.0;
+const stackBottomPadding = 80.0;
+BorderRadius radius = BorderRadius.circular(8.0);
+double productAspectRatio = 1 / 1.3;
