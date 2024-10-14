89a5fefa3bdf8f64b45b20ae04ea6a58d78bf820
pnk <pierre.nkoe@vema-eg.de>
2024-10-14 23:43:52 +0200

    Implementation du FoodAndDrinksScreen

---
+++ lib/screens/food_and_drinks_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart'; 
+
+class FoodAndDrinksScreen extends StatelessWidget {
+  const FoodAndDrinksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('FoodsAndDrinks'),
+      ),
+    );
+  }
+}
