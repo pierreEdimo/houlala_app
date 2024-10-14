bae2af1c442281ed0a42a29a918559dca1773233
pnk <pierre.nkoe@vema-eg.de>
2024-10-14 23:33:22 +0200

    Implementation du categories_state.freezed

---
+++ lib/features/categories/state/categories_state.freezed.dart
@@ -0,0 +1,189 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'categories_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$CategoriesState {
+  List<Categories> get categoriesList => throw _privateConstructorUsedError;
+  bool get loading => throw _privateConstructorUsedError;
+  String get errorMessage => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CategoriesStateCopyWith<CategoriesState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CategoriesStateCopyWith<$Res> {
+  factory $CategoriesStateCopyWith(
+          CategoriesState value, $Res Function(CategoriesState) then) =
+      _$CategoriesStateCopyWithImpl<$Res, CategoriesState>;
+  @useResult
+  $Res call(
+      {List<Categories> categoriesList, bool loading, String errorMessage});
+}
+
+/// @nodoc
+class _$CategoriesStateCopyWithImpl<$Res, $Val extends CategoriesState>
+    implements $CategoriesStateCopyWith<$Res> {
+  _$CategoriesStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? categoriesList = null,
+    Object? loading = null,
+    Object? errorMessage = null,
+  }) {
+    return _then(_value.copyWith(
+      categoriesList: null == categoriesList
+          ? _value.categoriesList
+          : categoriesList // ignore: cast_nullable_to_non_nullable
+              as List<Categories>,
+      loading: null == loading
+          ? _value.loading
+          : loading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      errorMessage: null == errorMessage
+          ? _value.errorMessage
+          : errorMessage // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$CategoriesStateImplCopyWith<$Res>
+    implements $CategoriesStateCopyWith<$Res> {
+  factory _$$CategoriesStateImplCopyWith(_$CategoriesStateImpl value,
+          $Res Function(_$CategoriesStateImpl) then) =
+      __$$CategoriesStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {List<Categories> categoriesList, bool loading, String errorMessage});
+}
+
+/// @nodoc
+class __$$CategoriesStateImplCopyWithImpl<$Res>
+    extends _$CategoriesStateCopyWithImpl<$Res, _$CategoriesStateImpl>
+    implements _$$CategoriesStateImplCopyWith<$Res> {
+  __$$CategoriesStateImplCopyWithImpl(
+      _$CategoriesStateImpl _value, $Res Function(_$CategoriesStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? categoriesList = null,
+    Object? loading = null,
+    Object? errorMessage = null,
+  }) {
+    return _then(_$CategoriesStateImpl(
+      categoriesList: null == categoriesList
+          ? _value._categoriesList
+          : categoriesList // ignore: cast_nullable_to_non_nullable
+              as List<Categories>,
+      loading: null == loading
+          ? _value.loading
+          : loading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      errorMessage: null == errorMessage
+          ? _value.errorMessage
+          : errorMessage // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$CategoriesStateImpl implements _CategoriesState {
+  _$CategoriesStateImpl(
+      {final List<Categories> categoriesList = const [],
+      this.loading = false,
+      this.errorMessage = ''})
+      : _categoriesList = categoriesList;
+
+  final List<Categories> _categoriesList;
+  @override
+  @JsonKey()
+  List<Categories> get categoriesList {
+    if (_categoriesList is EqualUnmodifiableListView) return _categoriesList;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_categoriesList);
+  }
+
+  @override
+  @JsonKey()
+  final bool loading;
+  @override
+  @JsonKey()
+  final String errorMessage;
+
+  @override
+  String toString() {
+    return 'CategoriesState(categoriesList: $categoriesList, loading: $loading, errorMessage: $errorMessage)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$CategoriesStateImpl &&
+            const DeepCollectionEquality()
+                .equals(other._categoriesList, _categoriesList) &&
+            (identical(other.loading, loading) || other.loading == loading) &&
+            (identical(other.errorMessage, errorMessage) ||
+                other.errorMessage == errorMessage));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(_categoriesList),
+      loading,
+      errorMessage);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$CategoriesStateImplCopyWith<_$CategoriesStateImpl> get copyWith =>
+      __$$CategoriesStateImplCopyWithImpl<_$CategoriesStateImpl>(
+          this, _$identity);
+}
+
+abstract class _CategoriesState implements CategoriesState {
+  factory _CategoriesState(
+      {final List<Categories> categoriesList,
+      final bool loading,
+      final String errorMessage}) = _$CategoriesStateImpl;
+
+  @override
+  List<Categories> get categoriesList;
+  @override
+  bool get loading;
+  @override
+  String get errorMessage;
+  @override
+  @JsonKey(ignore: true)
+  _$$CategoriesStateImplCopyWith<_$CategoriesStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
