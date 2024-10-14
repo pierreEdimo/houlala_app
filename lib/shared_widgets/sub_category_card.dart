b75af0252a15db417d7855223228860a9f84aa51
pnk <pierre.nkoe@vema-eg.de>
2024-10-15 00:01:56 +0200

    Implementation du SubCategoryCard

---
+++ lib/shared_widgets/sub_category_card.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../features/sub_categories/models/sub_category.dart';
+
+class SubCategoryCard extends StatelessWidget {
+  final SubCategory? subCategory;
+
+  const SubCategoryCard({
+    super.key,
+    this.subCategory,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Navigator.of(context)
+          .pushNamed('/subCategoryDetail', arguments: subCategory),
+      child: Card(
+        elevation: 0,
+        color: Colors.transparent,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(150.0),
+                  image: DecorationImage(
+                      image: NetworkImage(subCategory!.imageUrl!),
+                      fit: BoxFit.cover)),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width * 1,
+                color: const Color(0xFFf4efe8),
+                child: Center(
+                  child: Text(
+                    subCategory!.name!,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
