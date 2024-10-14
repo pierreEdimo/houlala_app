2da4978cef9bedd0b746284cffed559f5993b933
pnk <pierre.nkoe@vema-eg.de>
2024-10-15 00:01:30 +0200

    Implementation du SubCategory

---
+++ lib/features/sub_categories/models/sub_category.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../../categories/model/categories.dart';
+
+part 'sub_category.freezed.dart';
+part 'sub_category.g.dart';
+
+@freezed
+class SubCategory with _$SubCategory {
+  factory SubCategory({
+    int? id,
+    String? name,
+    String? imageUrl,
+    Categories? category,
+  }) = _SubCategory;
+
+  factory SubCategory.fromJson(Map<String, dynamic> json) => _$SubCategoryFromJson(json);
+}
