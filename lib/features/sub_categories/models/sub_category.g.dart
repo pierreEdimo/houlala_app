0b1cc9c7662ed3f67a9dbc7f116ac26e1d27ffaf
pnk <pierre.nkoe@vema-eg.de>
2024-10-15 00:01:47 +0200

    Implementation du SubCategoryG

---
+++ lib/features/sub_categories/models/sub_category.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sub_category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SubCategoryImpl _$$SubCategoryImplFromJson(Map<String, dynamic> json) =>
+    _$SubCategoryImpl(
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      imageUrl: json['imageUrl'] as String?,
+      category: json['category'] == null
+          ? null
+          : Categories.fromJson(json['category'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$SubCategoryImplToJson(_$SubCategoryImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'imageUrl': instance.imageUrl,
+      'category': instance.category,
+    };
