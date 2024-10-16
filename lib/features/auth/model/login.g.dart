36d5f23b5715d5131ce44b4fb02737a0a6b9e96b
pnk <pierre.nkoe@vema-eg.de>
2024-10-16 23:26:22 +0200

    objets generes.

---
+++ lib/features/auth/model/login.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$LoginImpl _$$LoginImplFromJson(Map<String, dynamic> json) => _$LoginImpl(
+      email: json['email'] as String?,
+      passWord: json['passWord'] as String?,
+    );
+
+Map<String, dynamic> _$$LoginImplToJson(_$LoginImpl instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'passWord': instance.passWord,
+    };
---
+++ lib/features/auth/model/user_token.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_token.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserTokenImpl _$$UserTokenImplFromJson(Map<String, dynamic> json) =>
+    _$UserTokenImpl(
+      token: json['token'] as String?,
+    );
+
+Map<String, dynamic> _$$UserTokenImplToJson(_$UserTokenImpl instance) =>
+    <String, dynamic>{
+      'token': instance.token,
+    };
