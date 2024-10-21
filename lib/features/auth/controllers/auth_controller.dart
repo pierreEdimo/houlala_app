d3e0906aa7fa88d17c1f7fe590e4fe2ef8c122cf
pnk <pierre.nkoe@vema-eg.de>
2024-10-22 00:45:50 +0200

    implementation de la methode getConnectedUser.

--- lib/features/auth/controllers/auth_controller.dart
+++ lib/features/auth/controllers/auth_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:houlala_app/features/auth/model/login.dart';
+import 'package:houlala_app/features/auth/model/user_model.dart';
 import 'package:houlala_app/features/auth/providers/auth_provider.dart';
 
 class AuthController {
@@ -11,11 +12,19 @@ class AuthController {
     ref!.read(authStateNotifierProvider.notifier).login(login);
   }
 
+  String get errorMessage {
+    return ref!.watch(authStateNotifierProvider).errorMessage;
+  }
+
   bool get isLoggedIn {
     return ref!.watch(authStateNotifierProvider).loggedIn;
   }
 
-  void checkConnectedUser(){
-    ref!.read(authStateNotifierProvider.notifier).checkAndSetConnectedUser();
+  bool get loading {
+    return ref!.watch(authStateNotifierProvider).loading;
+  }
+
+  UserModel? get connectedUser {
+    return ref!.watch(authStateNotifierProvider).connectedUser;
   }
 }
