dba74097a3dde3d2986a91525795c64397c8e77a
pnk <pierre.nkoe@vema-eg.de>
2024-10-28 21:09:50 +0100

    Ajout de nouvelles crud-methode.

--- lib/features/sellers/controllers/sellers_controller.dart
+++ lib/features/sellers/controllers/sellers_controller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../model/seller.dart';
-import '../provideries/seller_provider.dart';
+import '../providers/seller_provider.dart';
 
 class SellersController {
   WidgetRef? ref;
@@ -11,11 +11,27 @@ class SellersController {
     return ref!.watch(sellerStateNotifierProvider).sellerList;
   }
 
+  List<Seller> getSellerListByCategoryId(int categoryId){
+    return sellerList.where((seller) => seller.subCategory!.id == categoryId).toList();
+  }
+
+  List<Seller>  get filteredSellers {
+    return ref!.watch(sellerStateNotifierProvider).filteredSellerList;
+  }
+
   bool get loading {
     return ref!.watch(sellerStateNotifierProvider).loading;
   }
 
+  void searchSellers(String term, {int? subCategoryId}){
+    ref!.read(sellerStateNotifierProvider.notifier).searchSellers(term, subCategoryId: subCategoryId);
+  }
+
   String get errorMessage {
     return ref!.watch(sellerStateNotifierProvider).errorMessage;
+  }
+
+  void resetFilterSellers(){
+    ref!.read(sellerStateNotifierProvider.notifier).resetFilterSeller();
   }
 }
