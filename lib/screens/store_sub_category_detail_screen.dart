8c488c3820f012f6fafc939df82a6b571efb1cfd
pnk <pierre.nkoe@vema-eg.de>
2024-10-28 21:11:35 +0100

    Implmentation du StoreSubCategoryDetailScreen.

---
+++ lib/screens/store_sub_category_detail_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/args/category_args.dart';
+import 'package:houlala_app/features/sellers/controllers/sellers_controller.dart';
+import 'package:houlala_app/helpers/constants.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
+import 'package:houlala_app/shared_widgets/filter_button.dart';
+import 'package:houlala_app/shared_widgets/search_input.dart';
+import 'package:houlala_app/shared_widgets/seller_card.dart';
+
+import '../features/sellers/model/seller.dart';
+
+class StoreSubCategoryDetailScreen extends ConsumerWidget {
+  const StoreSubCategoryDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final categoryArg =
+        ModalRoute.of(context)!.settings.arguments as CategoryArg;
+
+    SellersController sellersController = SellersController(ref);
+
+    bool isLoading = sellersController.loading;
+    String errorMessage = sellersController.errorMessage;
+    List<Seller> sellerList = sellersController
+        .getSellerListByCategoryId(categoryArg.subCategory!.id!);
+
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: SearchInput(
+          subCategoryId: categoryArg.subCategory!.id!,
+          route: '/searchStore',
+          hinText: 'Rechercher dans ${categoryArg.categoryName}',
+        ),
+      ),
+      body: CustomContainer(
+        loading: isLoading,
+        errorMessage: errorMessage,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: verticalPadding,
+                    left: horizontalPadding,
+                    right: horizontalPadding,
+                    bottom: stackBottomPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      '${sellerList.length} Magasin(s) dans ${categoryArg.subCategory!.name}',
+                    ),
+                    GridView.count(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      crossAxisCount: 2,
+                      childAspectRatio: 1 / 1.2,
+                      children: sellerList
+                          .map((seller) => SellerCard(seller: seller))
+                          .toList(),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            sellerList.length > 1 ? const FilterButton() : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
