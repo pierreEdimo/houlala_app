48e71ae823133fa7994ea8f5c0df35019b17276a
pnk <pierre.nkoe@vema-eg.de>
2024-10-28 21:22:16 +0100

    ajout de subCategory dans le categoryArgs.

--- lib/args/category_args.dart
+++ lib/args/category_args.dart
@@ -1,6 +1,13 @@
+import 'package:houlala_app/features/sub_categories/models/sub_category.dart';
+
 class CategoryArg {
   final String? categoryName;
   final int? categoryId;
+  final SubCategory? subCategory;
 
-  CategoryArg({this.categoryId, this.categoryName});
+  CategoryArg({
+    this.categoryId,
+    this.categoryName,
+    this.subCategory,
+  });
 }
