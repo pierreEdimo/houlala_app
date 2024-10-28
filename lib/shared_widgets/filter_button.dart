f56d8173a97cfaf4e2fe701258b3e924d842b1f1
pnk <pierre.nkoe@vema-eg.de>
2024-10-28 21:21:18 +0100

    FilterProductsButton est maintenant FilterButton

--- lib/shared_widgets/filter_button.dart
+++ lib/shared_widgets/filter_button.dart
@@ -4,18 +4,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'c_button.dart';
 
-class FilterProductsButton extends ConsumerWidget {
-  const FilterProductsButton({super.key});
+class FilterButton extends ConsumerWidget {
+  final VoidCallback? onPressed;
+
+  const FilterButton({
+    super.key,
+    this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return  Align(
+    return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
         width: MediaQuery.of(context).size.width * .4,
         margin: const EdgeInsets.only(bottom: 20),
         child: CustomButton(
-          onTap: () {},
+          onTap: onPressed,
           leadingIcon: HeroIcons.adjustmentsHorizontal,
           title: 'Filtrer',
         ),
