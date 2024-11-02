58af4414f47b24c9e7c5aae2c6add1db55a5289f
pnk <pierre.nkoe@vema-eg.de>
2024-11-02 23:42:50 +0100

    Implementation du Profile_screen.

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
+import 'package:houlala_app/screens/logged_out_screen.dart';
+import 'package:houlala_app/screens/loggedin_screen.dart';
+
+class ProfileScreen extends ConsumerWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AuthController authController = AuthController(ref);
+    bool loading = authController.loading;
+    bool isLoggedIn = authController.isLoggedIn;
+
+    if (loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return Scaffold(
+      body: isLoggedIn ? const LoggedInScreen() : const LoggedOutScreen(),
+    );
+  }
+}
