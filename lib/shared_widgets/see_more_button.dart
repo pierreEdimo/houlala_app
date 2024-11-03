7967a879823e0a2e61de66b32a9e03bff585fffc
pnk <pierre.nkoe@vema-eg.de>
2024-11-04 00:02:42 +0100

    implementation du seemoreButton

---
+++ lib/shared_widgets/see_more_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SeeMoreButton extends StatelessWidget {
+  final VoidCallback? onTap;
+
+  const SeeMoreButton({
+    super.key,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomRight,
+      child: InkWell(
+        onTap: onTap,
+        child: const Text(
+          'Voir plus',
+          style: const TextStyle(color: Colors.blueAccent),
+        ),
+      ),
+    );
+  }
+}
