8b8eadd86018d142331a718309de661b28ee8a4d
pnk <pierre.nkoe@vema-eg.de>
2024-11-03 23:59:55 +0100

    ajout du topCategoryList

--- lib/features/categories/state/categories_state.dart
+++ lib/features/categories/state/categories_state.dart
@@ -5,7 +5,8 @@ part 'categories_state.freezed.dart';
 @freezed
 class CategoriesState with _$CategoriesState {
   factory CategoriesState({
-    @Default([]) List<Categories> categoriesList,
+    @Default([]) List<Categories> categoryList,
+    @Default([]) List<Categories> topCategoryList,
     @Default(false) bool loading,
     @Default('') String errorMessage,
   }) = _CategoriesState;
