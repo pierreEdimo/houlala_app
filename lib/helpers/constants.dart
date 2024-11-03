6d0e48985b0952d190844a721c646bf29aae2b54
pnk <pierre.nkoe@vema-eg.de>
2024-11-04 00:00:40 +0100

    augmentation du productRatio

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -4,7 +4,7 @@ const verticalPadding = 25.0;
 const horizontalPadding = 11.0;
 const stackBottomPadding = 80.0;
 BorderRadius radius = BorderRadius.circular(8.0);
-double productAspectRatio = 1 / 1.3;
+double productAspectRatio = 1/1.5;
 EdgeInsetsGeometry stackDefaultPadding = const EdgeInsets.only(
     top: verticalPadding,
     bottom: stackBottomPadding,
