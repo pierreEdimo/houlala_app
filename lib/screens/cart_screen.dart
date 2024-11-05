80b6aeaa219030dc15a537c158b5c11c284b500c
pnk <pierre.nkoe@vema-eg.de>
2024-11-05 20:40:03 +0100

    Lorsque l'utilisateur n'est pas connecte, le logged_out_cart_screen est affiche.

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
+import 'package:houlala_app/screens/logged_in_cart_screen.dart';
+import 'package:houlala_app/screens/logged_out_cart_screen.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends ConsumerWidget {
   const CartScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Carts'),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    AuthController authController = AuthController(ref);
+    bool loading = authController.loading;
+    bool loggedIn = authController.isLoggedIn;
+
+    if (loading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: loggedIn ? const LoggedInCartScreen() : const LoggedOutCartScreen(),
     );
   }
 }
