33d835057930a6e8af3429d32c3d69c138e4c30f
pnk <pierre.nkoe@vema-eg.de>
2024-11-05 20:40:22 +0100

    Implementation du checkout_button

---
+++ lib/shared_widgets/checkout_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/shared_widgets/c_button.dart';
+
+class CheckoutButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+
+  const CheckoutButton({
+    super.key,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        width: MediaQuery.of(context).size.width * .7,
+        margin: const EdgeInsets.only(bottom: 20),
+        child: CustomButton(
+          onTap: onPressed,
+          title: 'Procedez au paiement',
+          leadingIcon: HeroIcons.creditCard,
+        ),
+      ),
+    );
+  }
+}
