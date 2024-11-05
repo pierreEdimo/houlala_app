e27199c042083d311207df652b16b328b488c606
pnk <pierre.nkoe@vema-eg.de>
2024-11-05 20:55:58 +0100

    Implementation du mapped_cart_item_state.

---
+++ lib/features/carts/state/mapped_cart_item_state.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:houlala_app/features/carts/model/mapped_cart_item.dart';
+
+part 'mapped_cart_item_state.freezed.dart';
+
+@freezed
+class MappedCartItemState with _$MappedCartItemState {
+    factory MappedCartItemState({
+      @Default([]) List<MappedCartItem> cartItemList,
+      @Default('') String errorMessage,
+      @Default(false) bool loading,
+  }) = _MappedCartItemState;
+}
