7498d5c2cb1c469f637fc552e37fd4e2d4bd74aa
pnk <pierre.nkoe@vema-eg.de>
2024-11-10 23:22:55 +0100

    ajouter les methodes pour manipuler le panier.

--- lib/features/carts/controllers/cart_controller.dart
+++ lib/features/carts/controllers/cart_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:houlala_app/features/carts/model/create_cart_item.dart';
 import 'package:houlala_app/features/carts/model/mapped_cart_item.dart';
 import 'package:houlala_app/features/carts/providers/mapped_cart_item_provider.dart';
 
@@ -17,5 +18,23 @@ class CartController {
 
   String get errorMessage {
     return ref!.watch(cartItemStateNotifierProvider).errorMessage;
+  }
+
+  void addProductToCart(CreateCartItem createCartItem) {
+    ref!
+        .read(cartItemStateNotifierProvider.notifier)
+        .addProductToCart(createCartItem);
+  }
+
+  void increaseItemQuantity(int id) {
+    ref!.read(cartItemStateNotifierProvider.notifier).increaseItemQuantity(id);
+  }
+
+  void decreaseItemQuantity(int id) {
+    ref!.read(cartItemStateNotifierProvider.notifier).decreaseItemQuantity(id);
+  }
+
+  void removeProductFromCart(int id) {
+    ref!.read(cartItemStateNotifierProvider.notifier).removeProductFromCart(id);
   }
 }
