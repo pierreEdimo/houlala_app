9b5c81aaf16229b475ee0c76558542d35c3b23db
pnk <pierre.nkoe@vema-eg.de>
2024-12-22 17:42:51 +0100

    Refactoring

--- lib/screens/logged_out_cart_screen.dart
+++ lib/screens/logged_out_cart_screen.dart
@@ -1,12 +1,35 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
+import 'package:houlala_app/shared_widgets/checkout_button.dart';
 
 class LoggedOutCartScreen extends StatelessWidget {
   const LoggedOutCartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('logged out'),
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: Text(
+          'Panier',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          CheckoutButton(
+            onPressed: () => Navigator.of(context).pushNamed('/gastCheckout'),
+          )
+        ],
+      ),
     );
   }
 }
