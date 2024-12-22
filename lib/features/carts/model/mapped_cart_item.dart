ae99384933a620696d300012d4aef9a0d88a6a24
pnk <pierre.nkoe@vema-eg.de>
2024-12-22 15:19:10 +0100

    Refactoring

--- lib/features/carts/model/mapped_cart_item.dart
+++ lib/features/carts/model/mapped_cart_item.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:houlala_app/features/products/model/product.dart';
-import '../../sellers/model/seller.dart';
+import 'package:houlala_app/features/locals/model/local_model.dart';
+
+import 'cart_item.dart';
 
 part 'mapped_cart_item.freezed.dart';
 part 'mapped_cart_item.g.dart';
@@ -8,22 +9,9 @@ part 'mapped_cart_item.g.dart';
 @freezed
 class MappedCartItem with _$MappedCartItem{
     factory MappedCartItem({
-      Seller? seller,
+      LocalModel? local,
       List<CartItem>? cartItems
   }) = _MappedCartItem;
 
     factory MappedCartItem.fromJson(Map<String, dynamic> json) => _$MappedCartItemFromJson(json);
-}
-
-@freezed
-class CartItem with _$CartItem{
-    factory CartItem({
-      int? id,
-      String? userId,
-      Product? product,
-      int? quantity,
-      int? price
-  }) = _CartItem;
-
-  factory CartItem.fromJson(Map<String, dynamic> json) => _$CartItemFromJson(json);
 }
