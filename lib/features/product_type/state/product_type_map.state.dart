933124e9c6673955d98e5ac7c57b53f9f8a0f88b
pnk <pierre.nkoe@vema-eg.de>
2024-12-22 15:20:02 +0100

    Implementation du ProductTyoeMapState widget.

---
+++ lib/features/product_type/state/product_type_map.state.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:houlala_app/features/product_type/models/product_type.dart';
+
+part 'product_type_map.state.freezed.dart';
+
+@freezed
+class ProductTypeMapState with _$ProductTypeMapState {
+  factory ProductTypeMapState({
+    @Default(true) bool loading,
+    @Default('') String errorMessage,
+    @Default({}) Map<int, List<ProductType>>? productTypeMap,
+}) = _ProductTypeMapState;
+}
