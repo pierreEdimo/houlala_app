b49a351a5c40bee9aea0dfaa81b1aa18da9ffcfb
pnk <pierre.nkoe@vema-eg.de>
2024-12-22 17:45:15 +0100

    ToasTNotification Helper class a ete implementee.

---
+++ lib/helpers/ToastNotification.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:toastification/toastification.dart';
+
+class ToastNotification {
+  static void showSuccessFulAction(String message) {
+    toastification.show(
+      title: const Text('Succes'),
+      autoCloseDuration: const Duration(seconds: 3),
+      type: ToastificationType.success,
+      alignment: Alignment.bottomCenter,
+      style: ToastificationStyle.fillColored,
+      description: Text(message)
+    );
+  }
+
+  static void showErrorAction(String message) {
+    toastification.show(
+      title: const Text('Erreur'),
+      autoCloseDuration: const Duration(seconds: 3),
+      type: ToastificationType.error,
+      alignment: Alignment.bottomCenter,
+      style: ToastificationStyle.fillColored,
+      description: Text(message)
+    );
+  }
+}
