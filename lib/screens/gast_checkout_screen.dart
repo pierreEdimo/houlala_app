018fc89af90b81a7cccbedc58f21a014fa317548
pnk <pierre.nkoe@vema-eg.de>
2024-12-22 17:41:29 +0100

    Implementation du gast_checkout_screen

---
+++ lib/screens/gast_checkout_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
+
+class GastCheckoutScreen extends StatelessWidget {
+  const GastCheckoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: Text(
+          'Caisse',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            fontSize: 22
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text('Checkout is empty'),
+      ),
+    );
+
+  }
+}
