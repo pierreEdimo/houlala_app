5baa27f3106e838650f11dba39261166511113e0
pnk <pierre.nkoe@vema-eg.de>
2024-12-30 01:13:27 +0100

    Suppression de onTap

--- lib/shared_widgets/c_button.dart
+++ lib/shared_widgets/c_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
 
 class CustomButton extends StatelessWidget {
-  final Function()? onTap;
   final HeroIcons? leadingIcon;
   final String? title;
   final Color? color;
@@ -10,7 +9,6 @@ class CustomButton extends StatelessWidget {
 
   const CustomButton({
     super.key,
-    this.onTap,
     this.leadingIcon,
     this.title,
     this.color,
