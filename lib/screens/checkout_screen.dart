c73136da758a7aac9ff693cb05e2779d0ec28adf
pnk <pierre.nkoe@vema-eg.de>
2025-01-01 22:20:27 +0100

    refactoring du checkout_screen.

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/features/address/controllers/address_controller.dart';
+import 'package:houlala_app/features/address/model/address.dart';
+import 'package:houlala_app/features/carts/controllers/cart_controller.dart';
+import 'package:houlala_app/features/carts/model/mapped_cart_item.dart';
+import 'package:houlala_app/helpers/constants.dart';
+import 'package:houlala_app/shared_widgets/address_card.dart';
 import 'package:houlala_app/shared_widgets/c_app_bar.dart';
+import 'package:houlala_app/shared_widgets/c_card.dart';
+import 'package:houlala_app/shared_widgets/check_out_cart_item.dart';
+import 'package:houlala_app/shared_widgets/checkout_button.dart';
 
-class CheckoutScreen extends StatelessWidget {
+class CheckoutScreen extends ConsumerWidget {
   const CheckoutScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    CartController cartController = CartController(ref);
+    AddressController addressController = AddressController(ref);
+
+    Address? selectedAddress = addressController.getDeliveryAddress(1);
+    List<MappedCartItem> cartItems = cartController.cartItemList;
+
     return Scaffold(
       appBar: CustomAppBar(
         leading: IconButton(
@@ -16,14 +32,42 @@ class CheckoutScreen extends StatelessWidget {
         ),
         title: Text(
           'Caisse',
-          style: GoogleFonts.poppins(
-            fontWeight: FontWeight.bold,
-            fontSize: 22
-          ),
+          style: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 22),
         ),
       ),
-      body: const Center(
-        child: Text('Checkout is empty'),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: stackDefaultPadding,
+              child: Column(
+                spacing: verticalPadding,
+                children: [
+                  AddressCard(
+                    hasAddress: addressController.hasAddress(),
+                    selectedAddress: selectedAddress,
+                  ),
+                  CustomCard(
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      children: cartItems
+                          .map(
+                            (item) => CheckOutCartItem(
+                              mappedCartItem: item,
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          CheckoutButton(
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
