5adab9260c1721d39f2f24a6dc15f5903139c3d0
pnk <pierre.nkoe@vema-eg.de>
2025-01-01 14:14:07 +0100

    le model register est implemente.

---
+++ lib/features/auth/model/register.freezed.dart
@@ -0,0 +1,200 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'register.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Register _$RegisterFromJson(Map<String, dynamic> json) {
+  return _Register.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Register {
+  String? get email => throw _privateConstructorUsedError;
+  String? get userName => throw _privateConstructorUsedError;
+  String? get passWord => throw _privateConstructorUsedError;
+
+  /// Serializes this Register to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of Register
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $RegisterCopyWith<Register> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RegisterCopyWith<$Res> {
+  factory $RegisterCopyWith(Register value, $Res Function(Register) then) =
+      _$RegisterCopyWithImpl<$Res, Register>;
+  @useResult
+  $Res call({String? email, String? userName, String? passWord});
+}
+
+/// @nodoc
+class _$RegisterCopyWithImpl<$Res, $Val extends Register>
+    implements $RegisterCopyWith<$Res> {
+  _$RegisterCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of Register
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? userName = freezed,
+    Object? passWord = freezed,
+  }) {
+    return _then(_value.copyWith(
+      email: freezed == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String?,
+      userName: freezed == userName
+          ? _value.userName
+          : userName // ignore: cast_nullable_to_non_nullable
+              as String?,
+      passWord: freezed == passWord
+          ? _value.passWord
+          : passWord // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$RegisterImplCopyWith<$Res>
+    implements $RegisterCopyWith<$Res> {
+  factory _$$RegisterImplCopyWith(
+          _$RegisterImpl value, $Res Function(_$RegisterImpl) then) =
+      __$$RegisterImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String? email, String? userName, String? passWord});
+}
+
+/// @nodoc
+class __$$RegisterImplCopyWithImpl<$Res>
+    extends _$RegisterCopyWithImpl<$Res, _$RegisterImpl>
+    implements _$$RegisterImplCopyWith<$Res> {
+  __$$RegisterImplCopyWithImpl(
+      _$RegisterImpl _value, $Res Function(_$RegisterImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of Register
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? email = freezed,
+    Object? userName = freezed,
+    Object? passWord = freezed,
+  }) {
+    return _then(_$RegisterImpl(
+      email: freezed == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String?,
+      userName: freezed == userName
+          ? _value.userName
+          : userName // ignore: cast_nullable_to_non_nullable
+              as String?,
+      passWord: freezed == passWord
+          ? _value.passWord
+          : passWord // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$RegisterImpl implements _Register {
+  _$RegisterImpl({this.email, this.userName, this.passWord});
+
+  factory _$RegisterImpl.fromJson(Map<String, dynamic> json) =>
+      _$$RegisterImplFromJson(json);
+
+  @override
+  final String? email;
+  @override
+  final String? userName;
+  @override
+  final String? passWord;
+
+  @override
+  String toString() {
+    return 'Register(email: $email, userName: $userName, passWord: $passWord)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$RegisterImpl &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.userName, userName) ||
+                other.userName == userName) &&
+            (identical(other.passWord, passWord) ||
+                other.passWord == passWord));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, email, userName, passWord);
+
+  /// Create a copy of Register
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$RegisterImplCopyWith<_$RegisterImpl> get copyWith =>
+      __$$RegisterImplCopyWithImpl<_$RegisterImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$RegisterImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Register implements Register {
+  factory _Register(
+      {final String? email,
+      final String? userName,
+      final String? passWord}) = _$RegisterImpl;
+
+  factory _Register.fromJson(Map<String, dynamic> json) =
+      _$RegisterImpl.fromJson;
+
+  @override
+  String? get email;
+  @override
+  String? get userName;
+  @override
+  String? get passWord;
+
+  /// Create a copy of Register
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$RegisterImplCopyWith<_$RegisterImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/features/auth/model/register.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'register.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$RegisterImpl _$$RegisterImplFromJson(Map<String, dynamic> json) =>
+    _$RegisterImpl(
+      email: json['email'] as String?,
+      userName: json['userName'] as String?,
+      passWord: json['passWord'] as String?,
+    );
+
+Map<String, dynamic> _$$RegisterImplToJson(_$RegisterImpl instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'userName': instance.userName,
+      'passWord': instance.passWord,
+    };
