ed4a4a3f0398564ae4ea0f68e5b60e70addf65d6
pnk <pierre.nkoe@vema-eg.de>
2025-01-01 22:20:46 +0100

    refactoring du address_card.

--- lib/shared_widgets/address_card.dart
+++ lib/shared_widgets/address_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:houlala_app/features/address/model/address.dart';
 import 'package:houlala_app/shared_widgets/c_card.dart';
 import 'package:houlala_app/shared_widgets/column_headers.dart';
+import 'package:sizer/sizer.dart';
 
 class AddressCard extends StatelessWidget {
   final bool hasAddress;
@@ -16,18 +18,29 @@ class AddressCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomCard(
-      child: SizedBox(
-        height: 200,
+      child: Container(
+        constraints: const BoxConstraints(minHeight: 130),
         child: !hasAddress
-            ? Container(
-          padding: const EdgeInsets.all(8.0),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ColumnHeaders(title: 'Adresse de livraison')
-            ],
-          ),
-        )
+            ?  Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const ColumnHeaders(title: 'Adresse de livraison'),
+                SizedBox(
+                  height: 9.h,
+                  child:  InkWell(
+                    onTap: () {},
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        HeroIcon(HeroIcons.plus),
+                        Text("Ajouter votre adresse")
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
             : Container(),
       ),
     );
