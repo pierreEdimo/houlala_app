24713ca6b492fe1f5034aad8404e3d753429accd
pnk <pierre.nkoe@vema-eg.de>
2025-01-02 23:05:06 +0100

    implementation du ItemCalculations class Helper.

---
+++ lib/helpers/item_calculations.dart
@@ -0,0 +1,38 @@
+import 'package:houlala_app/features/carts/model/cart_item.dart';
+import 'package:houlala_app/features/carts/model/mapped_cart_item.dart';
+
+class ItemCalculations {
+  static double getTotalPrice(List<CartItem> items) {
+    double price = 0;
+    for (var item in items) {
+      price += item.price!;
+    }
+    return price;
+  }
+
+  static int getTotalQuantity(List<CartItem> items) {
+    int qty = 0;
+    for (var item in items) {
+      qty += item.quantity!;
+    }
+    return qty;
+  }
+
+  static double getOrderTotalPrice(List<MappedCartItem> mappedCartItems) {
+    double price = 0;
+    for (var mappedItem in mappedCartItems) {
+      double totalQuantity = getTotalPrice(mappedItem.cartItems!);
+      price += totalQuantity;
+    }
+    return price;
+  }
+
+  static int getOrderTotalQuantity(List<MappedCartItem> mappedCartItems) {
+    int quantity = 0;
+    for (var mappedItem in mappedCartItems) {
+      int totalQuantity = getTotalQuantity(mappedItem.cartItems!);
+      quantity += totalQuantity;
+    }
+    return quantity;
+  }
+}
