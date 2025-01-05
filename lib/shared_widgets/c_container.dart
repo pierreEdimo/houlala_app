d0fd94039b64c14946fd6a4425790c647ad0e5a4
pnk <pierre.nkoe@vema-eg.de>
2025-01-06 00:46:06 +0100

    le Builder ajoute pour faciliter la correction des erreurs.

--- lib/shared_widgets/c_container.dart
+++ lib/shared_widgets/c_container.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:houlala_app/helpers/toast_notification.dart';
 
-class CustomContainer extends StatefulWidget {
+class CustomContainer extends StatelessWidget {
   final bool? loading;
   final String? errorMessage;
   final Widget? child;
@@ -10,28 +10,19 @@ class CustomContainer extends StatefulWidget {
       {super.key, this.loading, this.errorMessage, this.child});
 
   @override
-  State<CustomContainer> createState() => _CustomContainerState();
-}
-
-class _CustomContainerState extends State<CustomContainer> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (widget.errorMessage!.isNotEmpty) {
-        ToastNotification.showErrorAction(widget.errorMessage!);
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (widget.loading != null && widget.loading == true) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
+    return Builder(builder: (context) {
+      if (errorMessage!.isNotEmpty) {
+        CustomToastNotification.showErrorAction(errorMessage!);
+      }
 
-    return widget.child ?? Container();
+      if (loading != null && loading == true) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+
+      return child ?? Container();
+    });
   }
 }
