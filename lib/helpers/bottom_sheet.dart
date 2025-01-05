6891003362785fbafcce21ec9206755de1f3e4b3
pnk <pierre.nkoe@vema-eg.de>
2025-01-06 00:49:07 +0100

    implementation du openEditForm et du openAddAddressForm.

--- lib/helpers/bottom_sheet.dart
+++ lib/helpers/bottom_sheet.dart
@@ -1,7 +1,14 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:houlala_app/features/address/controllers/address_controller.dart';
+import 'package:houlala_app/features/address/model/address.dart';
+import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
+import 'package:houlala_app/features/auth/model/edit_info.dart';
 import 'package:houlala_app/main.dart';
 import 'package:houlala_app/shared_widgets/c_button.dart';
+import 'package:houlala_app/shared_widgets/column_headers.dart';
+import 'package:houlala_app/shared_widgets/input_email.dart';
+import 'package:houlala_app/shared_widgets/input_text.dart';
 import 'package:sizer/sizer.dart';
 
 class CustomBottomSheet {
@@ -70,10 +77,219 @@ class CustomBottomSheet {
     );
   }
 
-  static void openSaveAddressForm() {
+  static void openEditInfosForm(
+    GlobalKey<FormState> formkey,
+    TextEditingController emailController,
+    TextEditingController firstNameController,
+    TextEditingController lastNameController,
+    TextEditingController phoneNumberController,
+    AuthController authController,
+  ) {
+    openBottomSheet(StatefulBuilder(
+      builder: (context, setState) {
+        return Form(
+          key: formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: ColumnHeaders(
+                  title: "Adresse de livraison",
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  spacing: 15,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      spacing: 10,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: InputText(
+                            placeholder: 'Nom',
+                            textEditingController: lastNameController,
+                            keyboardType: TextInputType.text,
+                            infoMessage: 'Inserez votre nom',
+                            mustFill: true,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: InputText(
+                            placeholder: 'Prenom',
+                            textEditingController: firstNameController,
+                            keyboardType: TextInputType.text,
+                            infoMessage: 'Inserez votre prenom',
+                            mustFill: true,
+                          ),
+                        )
+                      ],
+                    ),
+                    InputEmail(
+                      emailController: emailController,
+                    ),
+                    InputText(
+                      placeholder: 'Numero de telephone',
+                      textEditingController: phoneNumberController,
+                      keyboardType: TextInputType.text,
+                      infoMessage: 'Inserez votre Numero de telephone',
+                      mustFill: true,
+                    ),
+                    Row(
+                      spacing: 10,
+                      children: [
+                        Expanded(
+                          child: CustomButton(
+                            color: Colors.grey,
+                            onPressed: () => navigatorKey.currentState!.pop(),
+                            title: 'Annuller',
+                          ),
+                        ),
+                        Expanded(
+                          child: CustomButton(
+                            onPressed: () {
+                              if (formkey.currentState!.validate()) {
+                                EditInfo info = EditInfo(
+                                  email: emailController.text,
+                                  firstName: firstNameController.text,
+                                  lastName: lastNameController.text,
+                                  phoneNumber: phoneNumberController.text,
+                                );
+
+                                authController.editUserInfo(info);
+                              }
+                            },
+                            title: 'Enregistrer',
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    ), height: 50.h);
+  }
+
+  static void openSaveAddressForm(
+      GlobalKey<FormState> formkey,
+      TextEditingController streetController,
+      TextEditingController poBoxController,
+      TextEditingController cityController,
+      TextEditingController houseNumberController,
+      AddressController addressController,
+      {int? id}) {
     openBottomSheet(
-      Container(),
-      height: 50.h,
+      StatefulBuilder(builder: (context, setState) {
+        return Form(
+          key: formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: ColumnHeaders(
+                  title: "Adresse de livraison",
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  spacing: 15,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      spacing: 10,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: InputText(
+                            placeholder: 'Adresse',
+                            textEditingController: streetController,
+                            keyboardType: TextInputType.text,
+                            infoMessage: 'Inserez votre adresse',
+                            mustFill: true,
+                          ),
+                        ),
+                        Flexible(
+                          child: InputText(
+                            placeholder: 'Nr',
+                            textEditingController: houseNumberController,
+                            keyboardType: TextInputType.text,
+                            mustFill: false,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      spacing: 10,
+                      children: [
+                        Flexible(
+                            child: InputText(
+                          placeholder: 'Boite postalle',
+                          textEditingController: poBoxController,
+                          keyboardType: TextInputType.text,
+                          mustFill: false,
+                        )),
+                        Expanded(
+                          flex: 2,
+                          child: InputText(
+                            placeholder: 'Ville',
+                            textEditingController: cityController,
+                            keyboardType: TextInputType.text,
+                            mustFill: true,
+                            infoMessage: 'Inserez votre ville',
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      spacing: 10,
+                      children: [
+                        Expanded(
+                          child: CustomButton(
+                            color: Colors.grey,
+                            onPressed: () => navigatorKey.currentState!.pop(),
+                            title: 'Annuller',
+                          ),
+                        ),
+                        Expanded(
+                          child: CustomButton(
+                            onPressed: () {
+                              if (formkey.currentState!.validate()) {
+                                Address address = Address(
+                                    street: streetController.text,
+                                    poBox: poBoxController.text,
+                                    country: 'Cameroun',
+                                    houseNumber: houseNumberController.text,
+                                    city: cityController.text);
+                                if (id != null) {
+                                  addressController.editAddress(id, address);
+                                } else {
+                                  addressController.createAddress(address);
+                                }
+                              }
+                            },
+                            title: 'Enregistrer',
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      }),
+      height: 30.h,
     );
   }
 }
