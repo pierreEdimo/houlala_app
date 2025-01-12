b0aac304c57096b58f0178fbade319ed226bc78a
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:25:22 +0100

    implementation de la methode fetchUserOrders

--- lib/features/order/repositories/order_repository.dart
+++ lib/features/order/repositories/order_repository.dart
@@ -1,18 +1,36 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:houlala_app/features/order/model/order_model.dart';
+import 'package:houlala_app/helpers/token_helper.dart';
 import 'package:http/http.dart';
 
 class OrderRepository {
   Future<Response> placeOrder(OrderModel order) async {
     final Response response = await post(
-      Uri.parse('${dotenv.env['ORDER_URL']}'),
-      headers: <String, String> {
-        'Content-Type': 'application/json; charset=UTF-8'
-      }, 
-      body: jsonEncode(order)
-    );
+        Uri.parse('${dotenv.env['ORDER_URL']}'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(order));
     return response;
+  }
+
+  Future<List<OrderModel>> fetchUsersOrders() async {
+    String? token = await TokenHelper.getToken();
+    final Response response = await get(Uri.parse('${dotenv.env['ORDER_URL']}'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token'
+        });
+    if (response.statusCode == HttpStatus.ok) {
+      List<dynamic> body = jsonDecode(response.body);
+      List<OrderModel> orders =
+          body.map((dynamic order) => OrderModel.fromJson(order)).toList();
+      return orders;
+    } else {
+      throw 'no orders';
+    }
   }
 }
