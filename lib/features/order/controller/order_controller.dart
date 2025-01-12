54bd80d6dfc2ab8dc4d098def8ad860251c6c27b
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:23:30 +0100

    implementation des methodes du controller

--- lib/features/order/controller/order_controller.dart
+++ lib/features/order/controller/order_controller.dart
@@ -14,4 +14,16 @@ class OrderController {
       rethrow;
     }
   }
+
+  bool get loading {
+    return ref!.watch(orderStateNotifierProvider).loading;
+  }
+
+  List<OrderModel> get orderList {
+    return ref!.watch(orderStateNotifierProvider).orderList;
+  }
+
+  String get errorMessage {
+    return ref!.watch(orderStateNotifierProvider).errorMessage;
+  }
 }
