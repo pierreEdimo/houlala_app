28341ccaa266ddfc236cb34dadb990b5f0b30af0
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:34:30 +0100

    implementation des methodes dans le product_repository

--- lib/features/products/repositories/product_repository.dart
+++ lib/features/products/repositories/product_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:houlala_app/helpers/token_helper.dart';
 import 'package:http/http.dart';
 import '../model/product.dart';
 
@@ -8,6 +9,53 @@ class ProductRepository {
   Future<List<Product>> fetchProducts() async {
     String? uri = dotenv.env['PRODUCT_URL'];
     return _fetchProductFromDb(uri!);
+  }
+
+  Future<List<Product>> fetchFavoritesProducts() async {
+    String? token = await TokenHelper.getToken();
+    String? uri = '${dotenv.env['PRODUCT_URL']}/favorites';
+    return _fetchProductFromDb(uri, token: token);
+  }
+
+  Future<Response> addProductsToFavorite(int id) async {
+    String? token = await TokenHelper.getToken();
+    final Response response =
+        await post(Uri.parse('${dotenv.env['PRODUCT_URL']}/favorites/$id'),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+              'Authorization': 'Bearer $token'
+            },
+            body: null);
+    return response;
+  }
+
+  Future<Response> removeProductFromFavorite(int id) async {
+    String? token = await TokenHelper.getToken();
+    final Response response =
+        await delete(Uri.parse('${dotenv.env['PRODUCT_URL']}/favorites/$id'),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+              'Authorization': 'Bearer $token'
+            },
+            body: null);
+    return response;
+  }
+
+  Future<Product> fetchSingleProduct(int id) async {
+    String? token = await TokenHelper.getToken();
+    final Response response =
+        await get(Uri.parse('${dotenv.env['PRODUCT_URL']}/$id'),
+            headers: token != null
+                ? <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    'Authorization': 'Bearer $token'
+                  }
+                : null);
+    if(response.statusCode == HttpStatus.ok){
+      return Product.fromJson(jsonDecode(response.body));
+    } else {
+      throw "no product";
+    }
   }
 
   Future<List<Product>> searchProducts(String term,
@@ -28,8 +76,15 @@ class ProductRepository {
     return _fetchProductFromDb(uri);
   }
 
-  Future<List<Product>> _fetchProductFromDb(String uri) async {
-    final Response response = await get(Uri.parse(uri));
+  Future<List<Product>> _fetchProductFromDb(String uri, {String? token}) async {
+    final Response response = await get(Uri.parse(uri),
+        headers: token != null
+            ? <String, String>{
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Authorization': 'Bearer $token'
+              }
+            : null);
+
     if (response.statusCode == HttpStatus.ok) {
       List<dynamic> body = jsonDecode(response.body);
       List<Product> products =
