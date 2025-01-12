920b3079364ba07fe2e63dab150423871d549457
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:17:56 +0100

    Implementation du width en 90%.

--- lib/shared_widgets/filter_button.dart
+++ lib/shared_widgets/filter_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
+import 'package:sizer/sizer.dart';
 import 'c_button.dart';
 
 class FilterButton extends ConsumerWidget {
@@ -17,7 +18,7 @@ class FilterButton extends ConsumerWidget {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-        width: MediaQuery.of(context).size.width * .4,
+        width: 90.w,
         margin: const EdgeInsets.only(bottom: 20),
         child: CustomButton(
           onPressed: onPressed,
