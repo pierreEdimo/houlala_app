92ef82e73f09476e68ac064d8996781531b13663
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:22:03 +0100

    ajout des widgets dans le orders_screen.

--- lib/screens/my_orders_screen.dart
+++ lib/screens/my_orders_screen.dart
@@ -1,13 +1,57 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/features/order/controller/order_controller.dart';
+import 'package:houlala_app/features/order/model/order_model.dart';
+import 'package:houlala_app/helpers/constants.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
+import 'package:houlala_app/shared_widgets/filter_button.dart';
+import 'package:houlala_app/shared_widgets/order_card.dart';
 
-class MyOrdersScreen extends StatelessWidget {
+class MyOrdersScreen extends ConsumerWidget {
   const MyOrdersScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    OrderController orderController = OrderController(ref);
+
+    List<OrderModel> orderList = orderController.orderList;
+    bool loading = orderController.loading;
+    String errorMessage = orderController.errorMessage;
+
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: Text(
+          'Mes Commandes',
+          style: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+      ),
+      body: CustomContainer(
+        loading: loading,
+        errorMessage: errorMessage,
+        child: Stack(
+          children: [
+            ListView(
+              padding: stackDefaultPadding,
+              children: orderList
+                  .map(
+                    (order) => OrderCard(
+                      orderModel: order,
+                    ),
+                  )
+                  .toList(),
+            ),
+            FilterButton(
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
