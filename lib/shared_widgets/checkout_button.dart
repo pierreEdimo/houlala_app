444a3313f46f7534d5a5c4d8f91047c2780487c9
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:17:52 +0100

    Implementation du width en 90%.

--- lib/shared_widgets/checkout_button.dart
+++ lib/shared_widgets/checkout_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:houlala_app/shared_widgets/c_button.dart';
+import 'package:sizer/sizer.dart';
 
 class CheckoutButton extends StatelessWidget {
   final VoidCallback? onPressed;
@@ -15,7 +16,7 @@ class CheckoutButton extends StatelessWidget {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-        width: MediaQuery.of(context).size.width * .7,
+        width: 90.w,
         margin: const EdgeInsets.only(bottom: 20),
         child: CustomButton(
           onPressed: onPressed,
