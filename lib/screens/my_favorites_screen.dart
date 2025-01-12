fb2028b19d2af54834480ba79d02fc846703c0db
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:21:46 +0100

    ajout des widgets dans le favoryte_screen

--- lib/screens/my_favorites_screen.dart
+++ lib/screens/my_favorites_screen.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/features/products/controllers/product_controller.dart';
+import 'package:houlala_app/features/products/model/product.dart';
+import 'package:houlala_app/helpers/constants.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
+import 'package:houlala_app/shared_widgets/list_product_card.dart';
+import 'package:houlala_app/shared_widgets/product_list.dart';
 
-class MyFavoritesScreen extends StatelessWidget {
+class MyFavoritesScreen extends ConsumerWidget {
   const MyFavoritesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('My Favorites'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    ProductController productController = ProductController(ref);
+
+    List<Product> productList = productController.favoriteProductList;
+    bool loading = productController.loading;
+    String errorMessage = productController.errorMessage;
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: Text(
+          'Mes Favoris',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: CustomContainer(
+        loading: loading,
+        errorMessage: errorMessage,
+        child: productList.isNotEmpty
+            ? ProductList(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: horizontalPadding,
+                  vertical: verticalPadding,
+                ),
+                shrinkWrap: false,
+                productList: productList
+                    .map((product) => ListProductCard(
+                          product: product,
+                        ))
+                    .toList(),
+              )
+            : const Center(
+                child: Text('Pas de produit(s)'),
+              ),
       ),
     );
   }
