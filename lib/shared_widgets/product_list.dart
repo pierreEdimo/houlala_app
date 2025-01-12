4a61d41b6b6c9377c51551c807ae7ecf9a4fa9a8
pnk <pierre.nkoe@vema-eg.de>
2025-01-12 23:34:05 +0100

    attribut padding ajoute dans le productList.

--- lib/shared_widgets/product_list.dart
+++ lib/shared_widgets/product_list.dart
@@ -5,17 +5,20 @@ class ProductList extends StatelessWidget {
   final List<Widget>? productList;
   final bool? shrinkWrap;
   final ScrollPhysics? scrollPhysics;
+  final EdgeInsets? padding;
 
   const ProductList({
     super.key,
     this.productList,
     @Default(false) this.shrinkWrap,
     this.scrollPhysics,
+    this.padding,
   });
 
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: padding,
       shrinkWrap: shrinkWrap!,
       physics: scrollPhysics,
       children: productList!,
