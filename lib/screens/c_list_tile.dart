fa5773048b5fb552582e3e32b5f03a735f510463
pnk <pierre.nkoe@vema-eg.de>
2025-02-03 23:25:40 +0100

    Implémentation du CustomListTile

---
+++ lib/screens/c_list_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:houlala_app/helpers/constants.dart';
+
+class CustomListTile extends StatelessWidget {
+  final Widget? leading;
+  final Widget? title;
+  final Widget? suffix;
+  final VoidCallback? onTap;
+
+  const CustomListTile({
+    super.key,
+    this.title,
+    this.leading,
+    this.suffix,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: verticalPadding),
+        child: Row(
+          children: <Widget>[
+            Row(
+              spacing: 10,
+              children: <Widget>[leading ?? Container(), title ?? Container()],
+            ),
+            suffix ?? Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
