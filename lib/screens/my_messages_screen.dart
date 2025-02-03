f0e40a551ff7f2957915a7de4daa166e53f16f85
pnk <pierre.nkoe@vema-eg.de>
2025-02-03 23:34:34 +0100

    Implementation des mockscreens.

--- lib/screens/my_messages_screen.dart
+++ lib/screens/my_messages_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
 
 class MyMessagesScreen extends StatelessWidget {
   const MyMessagesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        'My Messages',
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: Text(
+          'Mes messages',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text("Vous n'avez aucuns messages."),
       ),
     );
   }
--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,13 +1,29 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Notification'),
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const HeroIcon(HeroIcons.chevronLeft),
+        ),
+        title: Text(
+          'Notifications',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text("Vous n'avez aucunes notifications."),
       ),
     );
   }
