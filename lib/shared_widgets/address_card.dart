9c78b158e5506d81b72209201ae951ce5948d2de
pnk <pierre.nkoe@vema-eg.de>
2025-02-03 23:23:32 +0100

    l'Adresscard est un stateful widget

--- lib/shared_widgets/address_card.dart
+++ lib/shared_widgets/address_card.dart
@@ -1,103 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:houlala_app/features/address/controllers/address_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:heroicons/heroicons.dart';
 import 'package:houlala_app/features/address/model/address.dart';
-import 'package:houlala_app/helpers/bottom_sheet.dart';
-import 'package:houlala_app/shared_widgets/add_info_button.dart';
+import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
+import 'package:houlala_app/helpers/constants.dart';
 import 'package:houlala_app/shared_widgets/c_card.dart';
-import 'package:houlala_app/shared_widgets/column_headers.dart';
 
-class AddressCard extends ConsumerStatefulWidget {
-  final bool hasAddress;
-  final Address? selectedAddress;
-  final bool? loading;
+class AddressCard extends ConsumerWidget {
+  final Address? address;
+  final VoidCallback? onEditPressed;
+  final VoidCallback? onDeletePressed;
 
   const AddressCard({
     super.key,
-    required this.hasAddress,
-    this.selectedAddress,
-    this.loading,
+    this.address,
+    this.onEditPressed,
+    this.onDeletePressed,
   });
 
   @override
-  ConsumerState<AddressCard> createState() => _AddressCardState();
-}
-
-class _AddressCardState extends ConsumerState<AddressCard> {
-  final TextEditingController streetController = TextEditingController();
-  final TextEditingController houseNumberController = TextEditingController();
-  final TextEditingController cityController = TextEditingController();
-  final TextEditingController poBoxController = TextEditingController();
-  final _formkey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    final AddressController addressController = AddressController(ref);
-
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AuthController authController = AuthController(ref);
+    var connectedUser = authController.connectedUser;
     return CustomCard(
       child: Container(
-        constraints: const BoxConstraints(minHeight: 80),
-        child: Builder(
-          builder: (context) {
-            if (widget.loading! || widget.selectedAddress == null) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return !widget.hasAddress
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+        constraints: const BoxConstraints(minHeight: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: verticalPadding,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  '${address?.lastName} ${address?.firstName}',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                    '${address?.street} ${address?.houseNumber}'),
+                Text('${address?.city} ${address!.poBox ?? ''}'),
+                Text('${address?.country}'),
+                connectedUser!.phoneNumber != null
+                    ? Row(
+                  children: [
+                    const Text('Numero de telephone: '),
+                    Text('${connectedUser.phoneNumber}')
+                  ],
+                )
+                    : Container()
+              ],
+            ),
+            Row(
+              spacing: horizontalPadding,
+              children: [
+                InkWell(
+                  onTap: onEditPressed,
+                  child: const Row(
+                    spacing: 5,
                     children: [
-                      const ColumnHeaders(title: 'Adresse de livraison'),
-                      AddInfoButton(
-                        onPressed: () => CustomBottomSheet.openSaveAddressForm(
-                            _formkey,
-                            streetController,
-                            poBoxController,
-                            cityController,
-                            houseNumberController,
-                            addressController),
-                        title: 'Ajoutez votre adresse',
-                      )
-                    ],
-                  )
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    spacing: 10,
-                    children: [
-                      const ColumnHeaders(title: 'Adresse de livraison'),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                              '${widget.selectedAddress?.street} ${widget.selectedAddress?.houseNumber}'),
-                          Text(
-                              '${widget.selectedAddress?.city} ${widget.selectedAddress?.poBox}, ${widget.selectedAddress?.country}'),
-                          Container(
-                            height: 20,
-                            alignment: Alignment.bottomLeft,
-                            child: InkWell(
-                              onTap: () =>
-                                  CustomBottomSheet.openSaveAddressForm(
-                                      _formkey,
-                                      streetController..text = widget.selectedAddress!.street!,
-                                      poBoxController..text = widget.selectedAddress!.poBox ?? '',
-                                      cityController..text = widget.selectedAddress!.city!,
-                                      houseNumberController..text = widget.selectedAddress!.houseNumber ?? '',
-                                      addressController,
-                                      id: widget.selectedAddress?.id),
-                              child: const Text(
-                                'Modifiez votre adresse',
-                                style: TextStyle(color: Colors.blue),
-                              ),
-                            ),
-                          )
-                        ],
+                      HeroIcon(
+                        HeroIcons.pencil,
+                        size: 18,
                       ),
+                      Text('Modifier'),
                     ],
-                  );
-          },
+                  ),
+                ),
+                InkWell(
+                  onTap: onDeletePressed,
+                  child: const Row(
+                    spacing: 5,
+                    children: [
+                      HeroIcon(
+                        HeroIcons.trash,
+                        size: 18,
+                      ),
+                      Text('Supprimer'),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
