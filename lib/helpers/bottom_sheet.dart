7e82448cc0c99b1be50595cd363e7d598394322d
pnk <pierre.nkoe@vema-eg.de>
2025-02-03 23:25:18 +0100

    Modification du bottomsheet

--- lib/helpers/bottom_sheet.dart
+++ lib/helpers/bottom_sheet.dart
@@ -4,6 +4,7 @@ import 'package:houlala_app/features/address/controllers/address_controller.dart
 import 'package:houlala_app/features/address/model/address.dart';
 import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
 import 'package:houlala_app/features/auth/model/edit_info.dart';
+import 'package:houlala_app/helpers/constants.dart';
 import 'package:houlala_app/main.dart';
 import 'package:houlala_app/shared_widgets/c_button.dart';
 import 'package:houlala_app/shared_widgets/column_headers.dart';
@@ -16,18 +17,26 @@ class CustomBottomSheet {
     Get.bottomSheet(
       Container(
         width: 100.w,
-        height: height ?? 15.h,
         color: Colors.transparent,
-        child: Container(
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(12.0),
-                  topRight: Radius.circular(12.0))),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: child,
-          ),
+        child: Wrap(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: horizontalPadding,
+                    right: horizontalPadding,
+                    bottom: verticalPadding),
+                child: child,
+              ),
+            ),
+          ],
         ),
       ),
       isDismissible: false,
@@ -85,49 +94,39 @@ class CustomBottomSheet {
     TextEditingController phoneNumberController,
     AuthController authController,
   ) {
-    openBottomSheet(StatefulBuilder(
-      builder: (context, setState) {
-        return Form(
-          key: formkey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 15),
-                child: ColumnHeaders(
-                  title: "Adresse de livraison",
+    openBottomSheet(
+      StatefulBuilder(
+        builder: (context, setState) {
+          return Form(
+            key: formkey,
+            child: Column(
+              spacing: 10,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 15),
+                  child: ColumnHeaders(
+                    title: "Informations personnelles",
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
+                Column(
                   spacing: 15,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: InputText(
-                            placeholder: 'Nom',
-                            textEditingController: lastNameController,
-                            keyboardType: TextInputType.text,
-                            infoMessage: 'Inserez votre nom',
-                            mustFill: true,
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: InputText(
-                            placeholder: 'Prenom',
-                            textEditingController: firstNameController,
-                            keyboardType: TextInputType.text,
-                            infoMessage: 'Inserez votre prenom',
-                            mustFill: true,
-                          ),
-                        )
-                      ],
+                    InputText(
+                      placeholder: 'Nom',
+                      textEditingController: lastNameController,
+                      keyboardType: TextInputType.text,
+                      infoMessage: 'Inserez votre nom',
+                      mustFill: true,
+                    ),
+                    InputText(
+                      placeholder: 'Prenom',
+                      textEditingController: firstNameController,
+                      keyboardType: TextInputType.text,
+                      infoMessage: 'Inserez votre prenom',
+                      mustFill: true,
                     ),
                     InputEmail(
                       emailController: emailController,
@@ -169,13 +168,13 @@ class CustomBottomSheet {
                       ],
                     )
                   ],
-                ),
-              )
-            ],
-          ),
-        );
-      },
-    ), height: 50.h);
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 
   static void openSaveAddressForm(
@@ -184,6 +183,8 @@ class CustomBottomSheet {
       TextEditingController poBoxController,
       TextEditingController cityController,
       TextEditingController houseNumberController,
+      TextEditingController lastNameController,
+      TextEditingController firstNameController,
       AddressController addressController,
       {int? id}) {
     openBottomSheet(
@@ -192,6 +193,7 @@ class CustomBottomSheet {
           key: formkey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            spacing: 10,
             children: [
               const Padding(
                 padding: EdgeInsets.only(top: 15),
@@ -199,97 +201,116 @@ class CustomBottomSheet {
                   title: "Adresse de livraison",
                 ),
               ),
-              Expanded(
-                child: Column(
-                  spacing: 15,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: InputText(
-                            placeholder: 'Adresse',
-                            textEditingController: streetController,
-                            keyboardType: TextInputType.text,
-                            infoMessage: 'Inserez votre adresse',
-                            mustFill: true,
-                          ),
+              Column(
+                spacing: 15,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  InputText(
+                    placeholder: 'Nom de famille',
+                    textEditingController: lastNameController,
+                    keyboardType: TextInputType.text,
+                    infoMessage: 'Inserez votre nom de famille',
+                    mustFill: true,
+                  ),
+                  InputText(
+                    placeholder: 'Prenom',
+                    textEditingController: firstNameController,
+                    keyboardType: TextInputType.text,
+                    infoMessage: 'Inserez votre prenom',
+                    mustFill: true,
+                  ),
+                  Row(
+                    spacing: 10,
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: InputText(
+                          placeholder: 'Adresse',
+                          textEditingController: streetController,
+                          keyboardType: TextInputType.text,
+                          infoMessage: 'Inserez votre adresse',
+                          mustFill: true,
                         ),
-                        Flexible(
-                          child: InputText(
-                            placeholder: 'Nr',
-                            textEditingController: houseNumberController,
-                            keyboardType: TextInputType.text,
-                            mustFill: false,
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Flexible(
-                            child: InputText(
-                          placeholder: 'Boite postalle',
-                          textEditingController: poBoxController,
+                      ),
+                      Flexible(
+                        child: InputText(
+                          placeholder: 'Nr',
+                          textEditingController: houseNumberController,
                           keyboardType: TextInputType.text,
                           mustFill: false,
-                        )),
-                        Expanded(
-                          flex: 2,
-                          child: InputText(
-                            placeholder: 'Ville',
-                            textEditingController: cityController,
-                            keyboardType: TextInputType.text,
-                            mustFill: true,
-                            infoMessage: 'Inserez votre ville',
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      spacing: 10,
-                      children: [
-                        Expanded(
-                          child: CustomButton(
-                            color: Colors.grey,
-                            onPressed: () => navigatorKey.currentState!.pop(),
-                            title: 'Annuller',
-                          ),
                         ),
-                        Expanded(
-                          child: CustomButton(
-                            onPressed: () {
-                              if (formkey.currentState!.validate()) {
-                                Address address = Address(
-                                    street: streetController.text,
-                                    poBox: poBoxController.text,
-                                    country: 'Cameroun',
-                                    houseNumber: houseNumberController.text,
-                                    city: cityController.text);
-                                if (id != null) {
-                                  addressController.editAddress(id, address);
-                                } else {
-                                  addressController.createAddress(address);
-                                }
+                      )
+                    ],
+                  ),
+                  Row(
+                    spacing: 10,
+                    children: [
+                      Flexible(
+                          child: InputText(
+                        placeholder: 'Boite postalle',
+                        textEditingController: poBoxController,
+                        keyboardType: TextInputType.text,
+                        mustFill: false,
+                      )),
+                      Expanded(
+                        flex: 2,
+                        child: InputText(
+                          placeholder: 'Ville',
+                          textEditingController: cityController,
+                          keyboardType: TextInputType.text,
+                          mustFill: true,
+                          infoMessage: 'Inserez votre ville',
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    spacing: 10,
+                    children: [
+                      Expanded(
+                        child: CustomButton(
+                          color: Colors.grey,
+                          onPressed: () => navigatorKey.currentState!.pop(),
+                          title: 'Annuller',
+                        ),
+                      ),
+                      Expanded(
+                        child: CustomButton(
+                          onPressed: () {
+                            if (formkey.currentState!.validate()) {
+                              Address address = Address(
+                                  firstName: firstNameController.text,
+                                  lastName: lastNameController.text,
+                                  street: streetController.text,
+                                  poBox: poBoxController.text,
+                                  country: 'Cameroun',
+                                  houseNumber: houseNumberController.text,
+                                  city: cityController.text);
+                              if (id != null) {
+                                addressController.editAddress(id, address);
+                              } else {
+                                addressController.createAddress(address);
                               }
-                            },
-                            title: 'Enregistrer',
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
+                              firstNameController.clear();
+                              lastNameController.clear();
+                              streetController.clear();
+                              poBoxController.clear();
+                              cityController.clear();
+                              houseNumberController.clear();
+                            }
+                          },
+                          title: 'Enregistrer',
+                        ),
+                      )
+                    ],
+                  )
+                ],
               )
             ],
           ),
         );
       }),
-      height: 30.h,
     );
   }
 }
