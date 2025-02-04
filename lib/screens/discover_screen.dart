b115eed2b7a78961f2ed5980ee98f29ead585c71
pnk <pierre.nkoe@vema-eg.de>
2025-02-04 22:51:02 +0100

    en cas d'erreur, rien ne s'affiche

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:houlala_app/features/category/controllers/category_controller.dart';
 import 'package:houlala_app/features/category/model/category_model.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
 import 'package:houlala_app/shared_widgets/vcategory_grid.dart';
 import '../helpers/constants.dart';
 import '../shared_widgets/column_headers.dart';
@@ -14,32 +15,34 @@ class DiscoverScreen extends ConsumerWidget {
     CategoryController categoriesController = CategoryController(ref);
     List<CategoryModel> categories = categoriesController.categoryList;
     bool loading = categoriesController.loading;
-    return Scaffold(
-      body: loading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: horizontalPadding,
-                  right: horizontalPadding,
-                  top: verticalPadding,
-                  bottom: 110,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const ColumnHeaders(
-                      title: 'Decouvrez les produits par Categories',
-                    ),
-                    VerticalCategoryGrid(
-                      categoryList: categories,
-                      shrinkWrap: true,
-                      aspectRatio: 1 / 1.1,
-                      physics: const ClampingScrollPhysics(),
-                    ),
-                  ],
+    return CustomContainer(
+      loading: loading,
+      errorMessage: categoriesController.errorMessage,
+      child: categoriesController.errorMessage.isNotEmpty
+          ? Container()
+          : Scaffold(
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: horizontalPadding,
+                    right: horizontalPadding,
+                    top: verticalPadding,
+                    bottom: 110,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const ColumnHeaders(
+                        title: 'Decouvrez les produits par Categories',
+                      ),
+                      VerticalCategoryGrid(
+                        categoryList: categories,
+                        shrinkWrap: true,
+                        aspectRatio: 1 / 1.1,
+                        physics: const ClampingScrollPhysics(),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
