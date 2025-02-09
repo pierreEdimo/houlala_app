e7d01d57c6088e24e0d50a6f4c1f373dbb0a8ba8
pnk <pierre.nkoe@vema-eg.de>
2025-02-09 15:36:10 +0100

    importations inutulusées suppriméés.

--- lib/shared_widgets/c_container.dart
+++ lib/shared_widgets/c_container.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:houlala_app/helpers/toast_notification.dart';
 import 'package:toastification/toastification.dart';
--- lib/shared_widgets/c_list_tile.dart
+++ lib/shared_widgets/c_list_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:houlala_app/helpers/constants.dart';
 
 class CustomListTile extends StatelessWidget {
   final Widget? leading;
