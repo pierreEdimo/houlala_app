ce83ef897466b29b0cf9d62318fc2f5570eb26be
pnk <pierre.nkoe@vema-eg.de>
2025-02-16 22:44:58 +0100

    implementation de fonctionnalite pour le gast.

--- lib/features/carts/controllers/cart_controller.dart
+++ lib/features/carts/controllers/cart_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:houlala_app/features/carts/model/cart_item.dart';
 import 'package:houlala_app/features/carts/model/mapped_cart_item.dart';
+import 'package:houlala_app/features/carts/providers/local_cart_item_provider.dart';
 import 'package:houlala_app/features/carts/providers/mapped_cart_item_provider.dart';
 
 class CartController {
@@ -10,6 +11,38 @@ class CartController {
 
   bool get loading {
     return ref!.watch(cartItemStateNotifierProvider).loading;
+  }
+
+  bool get isDbCartLoading {
+    return ref!.watch(localCartItemStateNotifierProvider).isCartLoading;
+  }
+
+  String get dbErrorMessage {
+    return ref!.watch(localCartItemStateNotifierProvider).errorMessage;
+  }
+
+  List<MappedCartItem> get mappedDbCartItemList {
+    var existingDbCartItemList =
+        ref!.watch(localCartItemStateNotifierProvider).cartItemList;
+
+    if (existingDbCartItemList.isNotEmpty) {
+      final Map<int, MappedCartItem> dbCartItemListMap = {};
+
+      for (final item in existingDbCartItemList) {
+        final localId = item.product?.local!.dbId;
+
+        dbCartItemListMap.putIfAbsent(
+          localId!,
+          () => MappedCartItem(local: item.product!.local!, cartItems: []),
+        );
+
+        dbCartItemListMap[localId] =
+            dbCartItemListMap[localId]!.copyWith(cartItems: [item]);
+      }
+      return dbCartItemListMap.values.toList();
+    }
+
+    return [];
   }
 
   List<MappedCartItem> get cartItemList {
@@ -26,10 +59,16 @@ class CartController {
         .emptiesTheMappedListAfterOrder();
   }
 
-  void addProductToCart(CartItem createCartItem) {
-    ref!
-        .read(cartItemStateNotifierProvider.notifier)
-        .addProductToCart(createCartItem);
+  void addProductToCart(CartItem createCartItem, {bool isLoggedIn = false}) {
+    if (isLoggedIn) {
+      ref!
+          .read(cartItemStateNotifierProvider.notifier)
+          .addProductToCart(createCartItem);
+    } else {
+      ref!
+          .read(localCartItemStateNotifierProvider.notifier)
+          .addProductToCartItem(createCartItem);
+    }
   }
 
   void increaseItemQuantity(int id) {
@@ -40,7 +79,15 @@ class CartController {
     ref!.read(cartItemStateNotifierProvider.notifier).decreaseItemQuantity(id);
   }
 
-  void removeProductFromCart(int id) {
-    ref!.read(cartItemStateNotifierProvider.notifier).removeProductFromCart(id);
+  void removeProductFromCart(CartItem item, {bool isLoggedIn = false}) {
+    if (isLoggedIn) {
+      ref!
+          .read(cartItemStateNotifierProvider.notifier)
+          .removeProductFromCart(item.dbId!);
+    } else {
+      ref!
+          .read(localCartItemStateNotifierProvider.notifier)
+          .removeItemFromCart(item);
+    }
   }
 }
