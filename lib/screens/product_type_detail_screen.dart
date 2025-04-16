d6e3258d3262992ec41448250d366db9860aaefd
pierre <pedimonkoe@yahoo.com>
2025-04-16 21:08:48 +0200

    Ajout du CustomContainer dans le product_type_detail_screen.

--- lib/screens/product_type_detail_screen.dart
+++ lib/screens/product_type_detail_screen.dart
@@ -3,13 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:houlala_app/args/category_args.dart';
 import 'package:houlala_app/helpers/search_args.dart';
-import 'package:houlala_app/shared_widgets/vproduct_grid.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
+import 'package:houlala_app/shared_widgets/c_scaffold.dart';
+import 'package:houlala_app/shared_widgets/product_grid.dart';
 import '../features/products/controllers/product_controller.dart';
 import '../features/products/model/product.dart';
 import '../features/product_type/models/product_type.dart';
 import '../helpers/constants.dart';
 import '../shared_widgets/c_app_bar.dart';
-import '../shared_widgets/filter_button.dart';
 import '../shared_widgets/search_input_button.dart';
 
 class ProductTypeDetailScreen extends ConsumerWidget {
@@ -25,8 +26,9 @@ class ProductTypeDetailScreen extends ConsumerWidget {
 
     ProductController productController = ProductController(ref);
     List<Product> productList = productController.getProductBySubCategoryId(categoryArg.categoryId!, productType!.dbId!);
+    bool loading = productController.loading;
 
-    return Scaffold(
+    return CustomScaffold(
       appBar: CustomAppBar(
         leading: IconButton(
           onPressed: () => Navigator.of(context).pop(),
@@ -44,29 +46,28 @@ class ProductTypeDetailScreen extends ConsumerWidget {
           ),
         ),
       ),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Padding(
-              padding: customDefaultPadding,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: CustomContainer(
+        loading: loading,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
                 children: [
                   Text(
                     '${productList.length} produits dans ${productType.name}',
                   ),
-                  VerticalProductGrid(
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    aspectRatio: productAspectRatio,
-                    productList: productList,
-                  )
                 ],
               ),
-            ),
+              VerticalProductGrid(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                aspectRatio: productAspectRatio,
+                productList: productList,
+              )
+            ],
           ),
-          productList.length > 1 ? const FilterButton() : Container()
-        ],
+        ),
       ),
     );
   }
