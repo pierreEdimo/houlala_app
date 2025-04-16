dac9397b8981b6e96f7053f65b7d34ba4bd4138f
pierre <pedimonkoe@yahoo.com>
2025-04-16 21:31:02 +0200

    implementation du ref en parametre.

--- lib/shared_widgets/login_tile.dart
+++ lib/shared_widgets/login_tile.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heroicons/heroicons.dart';
 
 import 'c_list_tile.dart';
 
-class LoginTile extends StatelessWidget {
+class LoginTile extends ConsumerWidget {
   final VoidCallback? onTap;
 
   const LoginTile({super.key, this.onTap});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
     return CustomListTile(
       onTap: () => Navigator.of(context).pushNamed('/login'),
       leading: const HeroIcon(
