9bbabc8a334c0bd6d8c01ebc795cfd350c0181af
pierre <pedimonkoe@yahoo.com>
2025-04-16 21:19:01 +0200

    ajout de la propriete color

--- lib/shared_widgets/c_app_bar.dart
+++ lib/shared_widgets/c_app_bar.dart
@@ -6,53 +6,47 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget? title;
   final Widget? leading;
   final List<Widget>? actions;
+  final Color? color;
 
-  const CustomAppBar({super.key, this.leading, this.title, this.actions});
+  const CustomAppBar({super.key, this.leading, this.title, this.actions, this.color});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
-      color: const Color(0xFFf4efe8),
+      color: color ?? const Color(0xFFf4efe8),
       child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 10.0),
-          child: Column(
-            children: [
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Expanded(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        leading != null ? leading! : Container(),
-                        leading != null && title != null
-                            ? const SizedBox(width: 10)
-                            : Container(),
-                        title != null ? Expanded(child: title!) : Container(),
-                      ],
-                    ),
-                  ),
-                  actions != null
-                      ? Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: actions!,
-                        ),
-                      )
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  leading != null ? leading! : Container(),
+                  leading != null && title != null
+                      ? const SizedBox(width: 10)
                       : Container(),
+                  title != null ? Expanded(child: title!) : Container(),
                 ],
               ),
-            ],
-          ),
+            ),
+            actions != null
+                ? Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: actions!,
+                  ),
+                )
+                : Container(),
+          ],
         ),
       ),
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(90.0);
+  Size get preferredSize => const Size.fromHeight(85);
 }
