5f8acc65195edab8eb8b89bec8d9a052d3052ef1
pierre <pedimonkoe@yahoo.com>
2025-04-16 21:10:49 +0200

    implementation de la methode isUrl.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -19,6 +19,10 @@ import 'package:houlala_app/shared_widgets/image_slider.dart';
 class ProductDetailScreen extends ConsumerWidget {
   const ProductDetailScreen({super.key});
 
+  bool isUrl(String imageUrl) {
+    return Uri.parse(imageUrl).isAbsolute;
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ProductController productController = ProductController(ref);
@@ -84,126 +88,142 @@ class ProductDetailScreen extends ConsumerWidget {
         ],
       ),
       body: CustomContainer(
-        errorMessage: productController.errorMessage,
-        loading: productController.loading,
-        child: selectedProduct != null
-            ? SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      bottom: 110,
-                      left: horizontalPadding,
-                      right: horizontalPadding,
-                      top: verticalPadding),
-                  child: Column(
+          loading: productController.loading,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 110),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Column(
+                    spacing: verticalPadding,
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Column(
-                        spacing: verticalPadding,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Container(
-                            height: 400,
-                            decoration: BoxDecoration(
-                                color: Colors.white, borderRadius: radius),
-                            child: ImageSlider(
-                              images: selectedProduct.images,
-                            ),
-                          ),
-                          Row(
-                            spacing: 10,
-                            children: [
-                              const HeroIcon(
-                                HeroIcons.buildingStorefront,
-                                size: 17,
-                              ),
-                              Text(
-                                selectedProduct.local!.name!,
-                                style: const TextStyle(
-                                  color: Colors.grey,
-                                  fontWeight: FontWeight.w500,
+                      selectedProduct != null
+                          ? Row(
+                              spacing: 8,
+                              children: [
+                                CircleAvatar(
+                                  radius: 18,
+                                  backgroundImage: isUrl(
+                                          selectedProduct.local!.imageUrl!)
+                                      ? NetworkImage(
+                                          selectedProduct.local!.imageUrl!)
+                                      : AssetImage(
+                                          'images/${selectedProduct.local!.imageUrl!}'),
                                 ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                      Text(
-                        selectedProduct.name!,
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.bold, fontSize: 22.0),
-                      ),
-                      Row(
-                        spacing: 20,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                '${selectedProduct.unitSellingPrice}',
-                                style: GoogleFonts.poppins(
-                                    fontWeight: FontWeight.bold, fontSize: 22),
-                              ),
-                              const Text(
-                                'XAF',
-                                style: TextStyle(
-                                  fontSize: 11,
-                                ),
-                              )
-                            ],
-                          ),
-                          StatefulBuilder(
-                            builder:
-                                (BuildContext context, StateSetter setState) {
-                              return Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () => setState(() {
-                                      if (quantity! > 1) {
-                                        quantity = quantity! - 1;
-                                      }
-                                    }),
-                                    icon: const HeroIcon(HeroIcons.minus),
-                                  ),
-                                  Text('$quantity'),
-                                  IconButton(
-                                      onPressed: () => setState(() {
-                                            quantity = quantity! + 1;
-                                          }),
-                                      icon: const HeroIcon(HeroIcons.plus))
-                                ],
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        spacing: verticalPadding,
-                        children: [
-                          Html(
-                            style: {
-                              "body": Style(
-                                  letterSpacing: 0.5,
-                                  lineHeight: LineHeight.em(1.3),
-                                  wordSpacing: 0.5)
-                            },
-                            data: selectedProduct.description,
-                          ),
-                          CustomButton(
-                            leadingIcon: HeroIcons.plus,
-                            onPressed: () =>
-                                addProductToCart(quantity!, isLoggedIn),
-                            title: 'Ajouter au panier',
-                          ),
-                        ],
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      selectedProduct.local!.name!,
+                                      style: GoogleFonts.poppins(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    InkWell(
+                                      onTap: () => Navigator.of(context)
+                                          .pushNamed('/store-detail',
+                                              arguments:
+                                                  selectedProduct.local!.dbId!),
+                                      child: const Text(
+                                        'Visiter le magasin',
+                                        style: TextStyle(
+                                            fontSize: 12, color: Colors.blue),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          : Container(),
+                      Container(
+                        height: 400,
+                        decoration: BoxDecoration(
+                            color: Colors.white, borderRadius: radius),
+                        child: ImageSlider(
+                          images: selectedProduct?.images,
+                        ),
                       ),
                     ],
                   ),
-                ),
-              )
-            : Container(),
-      ),
+                  Text(
+                    selectedProduct != null ? selectedProduct.name! : '',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    spacing: 20,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            '${selectedProduct?.unitSellingPrice}',
+                            style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          const Text(
+                            'XAF',
+                            style: TextStyle(
+                              fontSize: 11,
+                            ),
+                          )
+                        ],
+                      ),
+                      StatefulBuilder(
+                        builder: (BuildContext context, StateSetter setState) {
+                          return Row(
+                            children: [
+                              IconButton(
+                                onPressed: () => setState(() {
+                                  if (quantity! > 1) {
+                                    quantity = quantity! - 1;
+                                  }
+                                }),
+                                icon: const HeroIcon(HeroIcons.minus),
+                              ),
+                              Text('$quantity'),
+                              IconButton(
+                                  onPressed: () => setState(() {
+                                        quantity = quantity! + 1;
+                                      }),
+                                  icon: const HeroIcon(HeroIcons.plus))
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    spacing: verticalPadding,
+                    children: [
+                      Html(
+                        style: {
+                          "body": Style(
+                              letterSpacing: 0.5,
+                              lineHeight: LineHeight.em(1.3),
+                              wordSpacing: 0.5)
+                        },
+                        data: selectedProduct != null
+                            ? selectedProduct.description
+                            : '',
+                      ),
+                      CustomButton(
+                        leadingIcon: HeroIcons.plus,
+                        onPressed: () =>
+                            addProductToCart(quantity!, isLoggedIn),
+                        title: 'Ajouter au panier',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
