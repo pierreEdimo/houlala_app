ec6fa4dfe764c3d3a21e64541b07c683e2c9e287
pierre <pedimonkoe@yahoo.com>
2025-04-16 21:33:43 +0200

    le padding seulement en bottom et ajout du CustomContainer.

--- lib/screens/my_account_screen.dart
+++ lib/screens/my_account_screen.dart
@@ -5,6 +5,7 @@ import 'package:heroicons/heroicons.dart';
 import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
 import 'package:houlala_app/features/auth/model/user_model.dart';
 import 'package:houlala_app/helpers/constants.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
 import 'package:houlala_app/shared_widgets/c_list_tile.dart';
 import 'package:houlala_app/shared_widgets/c_app_bar.dart';
 import 'package:houlala_app/shared_widgets/c_card.dart';
@@ -37,97 +38,96 @@ class MyAccountScreen extends ConsumerWidget {
         ),
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: horizontalPadding,
-          vertical: verticalPadding,
-        ),
-        child: Column(
-          spacing: verticalPadding,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const ColumnHeaders(
-                  title: 'Informations personnelles',
-                ),
-                CustomCard(
-                  child: Column(
-                    children: [
-                      const UserInfoTile(),
-                      CustomListTile(
-                        onTap: () =>
-                            Navigator.of(context).pushNamed('/myAddresses'),
-                        leading: const Icon(
-                          Icons.book_outlined,
-                          size: 18,
+        padding: const EdgeInsets.only(bottom: 110),
+        child: CustomContainer(
+          child: Column(
+            spacing: verticalPadding,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const ColumnHeaders(
+                    title: 'Informations personnelles',
+                  ),
+                  CustomCard(
+                    child: Column(
+                      children: [
+                        const UserInfoTile(),
+                        CustomListTile(
+                          onTap: () =>
+                              Navigator.of(context).pushNamed('/myAddresses'),
+                          leading: const Icon(
+                            Icons.book_outlined,
+                            size: 18,
+                          ),
+                          title: const Text('Mes adresses'),
                         ),
-                        title: const Text('Mes adresses'),
-                      ),
-                      CustomListTile(
-                        onTap: () {},
-                        leading: const HeroIcon(
-                          HeroIcons.lockClosed,
-                          size: 18,
+                        CustomListTile(
+                          onTap: () {},
+                          leading: const HeroIcon(
+                            HeroIcons.lockClosed,
+                            size: 18,
+                          ),
+                          title: const Text('Changer votre mot de passe'),
                         ),
-                        title: const Text('Changer votre mot de passe'),
-                      ),
-                      CustomListTile(
-                        onTap: () {},
-                        leading: const HeroIcon(
-                          HeroIcons.banknotes,
-                          size: 18,
+                        CustomListTile(
+                          onTap: () {},
+                          leading: const HeroIcon(
+                            HeroIcons.banknotes,
+                            size: 18,
+                          ),
+                          title: const Text('Vendre sur Houla la'),
                         ),
-                        title: const Text('Vendre sur Houla la'),
-                      ),
-                      CustomListTile(
-                        onTap: () {},
-                        leading: const HeroIcon(
-                          HeroIcons.trash,
-                          size: 18,
-                          color: Colors.red,
-                        ),
-                        title: const Text(
-                          'Supprimer votre compte',
-                          style: TextStyle(
+                        CustomListTile(
+                          onTap: () {},
+                          leading: const HeroIcon(
+                            HeroIcons.trash,
+                            size: 18,
                             color: Colors.red,
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const ColumnHeaders(
-                  title: 'Notifications par E-mail',
-                ),
-                CustomCard(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          text:
-                              "tous les courriels seront envoyés à l'adresse suivante: ",
-                          style: GoogleFonts.poppins(color: Colors.black),
-                          children: [
-                            TextSpan(
-                              text: currentUser!.email!,
-                              style: GoogleFonts.poppins(
-                                  fontWeight: FontWeight.bold),
+                          title: const Text(
+                            'Supprimer votre compte',
+                            style: TextStyle(
+                              color: Colors.red,
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const ColumnHeaders(
+                    title: 'Notifications par E-mail',
                   ),
-                )
-              ],
-            )
-          ],
+                  CustomCard(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text:
+                                "tous les courriels seront envoyés à l'adresse suivante: ",
+                            style: GoogleFonts.poppins(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: currentUser!.email!,
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
