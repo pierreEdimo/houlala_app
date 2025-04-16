17b7f9abd4a871bea3decdcee9b568aa4eb4f757
pierre <pedimonkoe@yahoo.com>
2025-04-16 21:30:46 +0200

    CustomContainer implemente

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:heroicons/heroicons.dart';
 import 'package:houlala_app/features/auth/controllers/auth_controller.dart';
 import 'package:houlala_app/features/auth/model/login.dart';
+import 'package:houlala_app/features/main_nav/main_nav_providers.dart';
 import 'package:houlala_app/helpers/constants.dart';
+import 'package:houlala_app/shared_widgets/c_app_bar.dart';
 import 'package:houlala_app/shared_widgets/c_button.dart';
-import 'package:houlala_app/shared_widgets/c_card.dart';
+import 'package:houlala_app/shared_widgets/c_container.dart';
 import 'package:houlala_app/shared_widgets/input_email.dart';
 import 'package:houlala_app/shared_widgets/input_password.dart';
 
@@ -29,90 +32,94 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     AuthController authController = AuthController(ref);
     return Scaffold(
       resizeToAvoidBottomInset: true,
+      appBar: const CustomAppBar(),
       body: SafeArea(
         child: LayoutBuilder(builder: (context, constraints) {
           return SingleChildScrollView(
-            padding: const EdgeInsets.only(top: 30.0),
             child: ConstrainedBox(
               constraints: BoxConstraints(minHeight: constraints.minHeight),
               child: IntrinsicHeight(
-                child: Form(
-                  key: _formkey,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: horizontalPadding,
-                    ),
+                child: CustomContainer(
+                  child: Form(
+                    key: _formkey,
                     child: Column(
+                      spacing: 25,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Center(
-                          child: Image.asset(
-                            'images/houlala1.png',
-                            height: 150,
+                          child: Text(
+                            'Houla la',
+                            style: GoogleFonts.pacifico(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 32,
+                            ),
                           ),
                         ),
-                        CustomCard(
-                          child: Column(
-                            spacing: verticalPadding,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Column(
-                                spacing: 10,
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  InputEmail(
-                                    emailController: emailController,
-                                  ),
-                                  InputPassword(
-                                    passwordController: passwordController,
-                                    passwordRex: passWordRex,
-                                  )
-                                ],
-                              ),
-                              InkWell(
-                                onTap: () => {},
-                                child: const Text(
-                                  "Mot de Passe oublie? / Probleme de connexion?",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                        Column(
+                          spacing: verticalPadding,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Column(
+                              spacing: 10,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                InputEmail(
+                                  emailController: emailController,
+                                ),
+                                InputPassword(
+                                  passwordController: passwordController,
+                                  passwordRex: passWordRex,
+                                )
+                              ],
+                            ),
+                            InkWell(
+                              onTap: () => {},
+                              child: const Text(
+                                "Mot de Passe oublie? / Probleme de connexion?",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Column(
-                                spacing: 10,
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  CustomButton(
-                                    onPressed: () {
-                                      if (_formkey.currentState!.validate()) {
-                                        Login login = Login(
-                                            email: emailController.text,
-                                            passWord: passwordController.text);
+                            ),
+                            Column(
+                              spacing: 10,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                CustomButton(
+                                  onPressed: () {
+                                    if (_formkey.currentState!.validate()) {
+                                      Login login = Login(
+                                          email: emailController.text,
+                                          passWord: passwordController.text);
 
-                                        authController.login(login);
-                                      }
-                                    },
-                                    leadingIcon:
-                                        HeroIcons.arrowRightEndOnRectangle,
-                                    color: Colors.orange,
-                                    title: 'Se Connecter',
-                                  ),
-                                  Row(
-                                    children: [
-                                      const Text('Vous etes nouveau?'),
-                                      TextButton(
-                                        onPressed: () => Navigator.of(context)
-                                            .pushNamed("/logup"),
-                                        child: const Text(
-                                          'S\'inscrire',
-                                        ),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
+                                      authController.login(login);
+
+                                      ref
+                                          .read(mainNavStateNotifierProvider
+                                              .notifier)
+                                          .onItemTaped(0);
+                                    }
+                                  },
+                                  leadingIcon:
+                                      HeroIcons.arrowRightEndOnRectangle,
+                                  color: Colors.orange,
+                                  title: 'Se Connecter',
+                                ),
+                                Row(
+                                  children: [
+                                    const Text('Nouveau?'),
+                                    TextButton(
+                                      onPressed: () => Navigator.of(context)
+                                          .pushNamed("/logup"),
+                                      child: const Text(
+                                        'Creer un compte',
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
                         ),
                       ],
                     ),
